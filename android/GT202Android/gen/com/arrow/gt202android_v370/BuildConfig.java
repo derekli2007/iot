1f1e8f0e1e9ac105a376afc08e3ef0407ebd0054
unknown <derekli@cnlw7sz1derekli.ap.corp.arrow.com>
2014-07-25 10:57:10 +0800

    commit revision GT202Android_Add_Spinner_20140724 by derek

---
+++ android/GT202Android/gen/com/arrow/gt202android_v370/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.arrow.gt202android_v370;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ android/GT202Android/src/com/arrow/gt202android/DVDRemote.java
@@ -0,0 +1,6 @@
+package com.arrow.gt202android;
+
+public class DVDRemote {
+	
+	
+}
